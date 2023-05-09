9d5aea16a16eb66a5c589eaee195fb507acb047a
Gheylla <104219161+Gheylla@users.noreply.github.com>
2023-05-09 14:36:45 +0200

    Update test.py

--- test.py
+++ test.py
@@ -18,35 +18,41 @@ import cloudmetrics
 from skimage.transform import resize
 from PIL import Image 
 import netCDF4 as nc
+import os
 
 '''Step 1: Import data'''
 print('Step 1: Import data')
 '''For this specific case we are using low cloud cover fields from HARMONIE outputs'''
 
-path_dataset = r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\data\cut_sat\OR_ABI-L2-CMIPF-M6C13_G16_s20200181110194_e20200181119514_c20200181120008.nc'
+#path_dataset = r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\data\cut_sat\OR_ABI-L2-CMIPF-M6C13_G16_s20200181110194_e20200181119514_c20200181120008.nc'
+path = '/desktop/tu_delft/data/'
+name = 'OR_ABI-L2-CMIPF-M6C13_G16_s20200010000216_e20200010009536_c20200010010028.nc'
+path_dataset = os.path.join(path,name)
+
+print(path_dataset)
 dataset = xr.open_mfdataset(path_dataset, combine = 'by_coords', engine = 'netcdf4')
 
 
 
 #with x and y you get the whole space filled with values, with lat and lon you get nan values
-plt.figure()
-dataset = dataset.isel(time = 0)
+#plt.figure()
+#dataset = dataset.isel(time = 0)
 
-scene = dataset.C13_Brightness_Temp
-plt.contourf(dataset.lon.values, dataset.lat.values, dataset.C13_Brightness_Temp.values)
+#scene = dataset.C13_Brightness_Temp
+#plt.contourf(dataset.lon.values, dataset.lat.values, dataset.C13_Brightness_Temp.values)
 
-imin = np.argmin(scene.shape)
-scene = resize(scene, (scene.shape[imin],scene.shape[imin]), anti_aliasing=True)
+#imin = np.argmin(scene.shape)
+#scene = resize(scene, (scene.shape[imin],scene.shape[imin]), anti_aliasing=True)
 
-assert(np.isnan(scene).any() == False)
+#assert(np.isnan(scene).any() == False)
 
 
-vmin = 270
-vmax = 300
-cmap = "RdBu_r"
-plt.figure()
-plt.imshow(scene, cmap=cmap, vmin=vmin, vmax=vmax)
-plt.show()
+#vmin = 270
+#vmax = 300
+#cmap = "RdBu_r"
+#plt.figure()
+#plt.imshow(scene, cmap=cmap, vmin=vmin, vmax=vmax)
+#plt.show()
 # =============================================================================
 # 
 # plt.figure()
