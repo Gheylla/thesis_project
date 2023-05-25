45a6f6a12ad09b9d8ff1144b6cc9f0ef5c466ac9
Gheylla <104219161+Gheylla@users.noreply.github.com>
2023-05-25 14:25:41 +0200

    Update validate_metrics_ghvshau.py

--- validate_metrics_ghvshau.py
+++ validate_metrics_ghvshau.py
@@ -22,20 +22,26 @@ import seaborn as sn
 
 # %%
 '''Import the data'''
-metrics_mart = pd.read_hdf(r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\metric_results\Observations\MetricsRef1_MARTINpaper.h5')
-metrics_hauk = xr.open_mfdataset(r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\metric_results\Observations\GOES16_IR_nc_Iorg_EUREC4A_10-20_-58--48.nc')
-metrics_gheyl = pd.read_hdf(r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\metric_results\observations\complete_observations.h5')
+#metrics_mart = pd.read_hdf(r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\metric_results\Observations\MetricsRef1_MARTINpaper.h5')
+metrics_hauk = xr.open_mfdataset(r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\metric_results\Observations\GOES16_IR_nc_Iorg_EUREC4A_10-20_-58--48.nc') #https://www.zenodo.org/record/5979718#.ZGHwp3ZBxD8
+metrics_gheyl_280290 = pd.read_hdf(r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\metric_results\observations\complete_observations_280290.h5')
+metrics_gheyl_277290 = pd.read_hdf(r'C:\Users\LENOVO\Desktop\TU_Delft\thesis\metric_results\observations\complete_observations_277290.h5')
 
 
 #%%
 '''Compute the diurnal cycle of the metrics'''
-diur_gheyl_iorg = metrics_gheyl.groupby([metrics_gheyl.index.hour])['iorg'].mean()
+
+
+diur_gheyl_iorg_280290 = metrics_gheyl_280290.groupby([metrics_gheyl_280290.index.hour])['iorg'].mean()
+diur_gheyl_iorg_277290 = metrics_gheyl_277290.groupby([metrics_gheyl_277290.index.hour])['iorg'].mean()
 diur_hauk_iorg = metrics_hauk.Iorg.groupby(metrics_hauk.time.dt.hour).mean()
 
-diur_gheyl_num = metrics_gheyl.groupby([metrics_gheyl.index.hour])['num_objects'].mean()
+diur_gheyl_num_280290 = metrics_gheyl_280290.groupby([metrics_gheyl_280290.index.hour])['num_objects'].mean()
+diur_gheyl_num_277290 = metrics_gheyl_277290.groupby([metrics_gheyl_277290.index.hour])['num_objects'].mean()
 diur_hauk_num = metrics_hauk.N.groupby(metrics_hauk.time.dt.hour).mean()
 
-diur_gheyl_cf = metrics_gheyl.groupby([metrics_gheyl.index.hour])['cloud_fraction'].mean()
+diur_gheyl_cf_280290 = metrics_gheyl_280290.groupby([metrics_gheyl_280290.index.hour])['cloud_fraction'].mean()
+diur_gheyl_cf_277290 = metrics_gheyl_277290.groupby([metrics_gheyl_277290.index.hour])['cloud_fraction'].mean()
 diur_hauk_cf = metrics_hauk.cloud_fraction.groupby(metrics_hauk.time.dt.hour).mean()
 
 diur_hauk_size = metrics_hauk.cluster_size_mean.groupby(metrics_hauk.time.dt.hour).mean()
@@ -43,27 +49,38 @@ diur_hauk_size = metrics_hauk.cluster_size_mean.groupby(metrics_hauk.time.dt.hou
 # %%
 '''Plot graphs of time series'''
 plt.figure(figsize = (25,10))
-plt.plot(metrics_hauk.time.values, metrics_hauk.Iorg.values, label = 'Hauke CONT3XT data')
-plt.plot(metrics_gheyl.Time.values[1:8879], metrics_gheyl.iorg.values[1:8879], label = 'Gheylla GOES-16 data')
+plt.plot(metrics_hauk.time.values, metrics_hauk.Iorg.values, label = 'Hauke CONT3XT data', color = 'red')
+
+
+
+plt.plot(metrics_gheyl_280290.Time.values[1:8879], metrics_gheyl_280290.iorg.values[1:8879], label = 'Gheylla GOES-16 data mask 280-290K', color = 'black')
+plt.plot(metrics_gheyl_277290.Time.values[1:8879], metrics_gheyl_277290.iorg.values[1:8879], label = 'Gheylla GOES-16 data mask 277-290K', color = 'black', linestyle = 'dashed')
 plt.title('Timeseries of Iorg')
 plt.xlabel('Time')
 plt.ylabel('Iorg [-]')
 plt.grid()
 plt.legend()
 
-
+#%%
 plt.figure(figsize = (25,10))
-plt.plot(metrics_hauk.time.values, metrics_hauk.N.values, label = 'Hauke CONT3XT data')
-plt.plot(metrics_gheyl.Time.values[1:8879], metrics_gheyl.num_objects.values[1:8879], label = 'Gheylla GOES-16 data')
+plt.plot(metrics_hauk.time.values, metrics_hauk.N.values, label = 'Hauke CONT3XT data', color = 'red')
+
+plt.plot(metrics_gheyl_280290.Time.values[1:8879], metrics_gheyl_280290.num_objects.values[1:8879], label = 'Gheylla GOES-16 data mask 280-290K', color = 'black')
+plt.plot(metrics_gheyl_277290.Time.values[1:8879], metrics_gheyl_277290.num_objects.values[1:8879], label = 'Gheylla GOES-16 data mask 277-290K', color = 'black',  linestyle = 'dashed')
 plt.title('Timeseries of number of objects')
 plt.xlabel('Time')
 plt.ylabel('Num Objects [-]')
 plt.grid()
 plt.legend()
 
+#%%
+
 plt.figure(figsize = (25,10))
-plt.plot(metrics_hauk.time.values, metrics_hauk.cloud_fraction.values, label = 'Hauke CONT3XT data')
-plt.plot(metrics_gheyl.Time.values[1:8879], metrics_gheyl.cloud_fraction.values[1:8879], label = 'Gheylla GOES-16 data')
+plt.plot(metrics_hauk.time.values, metrics_hauk.cloud_fraction.values, label = 'Hauke CONT3XT data', color = 'red')
+
+
+plt.plot(metrics_gheyl_280290.Time.values[1:8879], metrics_gheyl_280290.cloud_fraction.values[1:8879], label = 'Gheylla GOES-16 data mask 280-290K', color = 'black')
+plt.plot(metrics_gheyl_277290.Time.values[1:8879], metrics_gheyl_277290.cloud_fraction.values[1:8879], label = 'Gheylla GOES-16 data mask 277-290K', color = 'black', linestyle = 'dashed')
 plt.title('Timeseries of cloud fraction')
 plt.xlabel('Time')
 plt.ylabel('CF[-]')
@@ -73,52 +90,69 @@ plt.legend()
 
 # %%
 '''Plot graphs of diurnal cycle'''
-plt.figure(figsize = (25,10))
-plt.plot(diur_hauk_iorg.hour.values, diur_hauk_iorg, label = 'Hauke CONT3XT data')
-plt.plot(diur_gheyl_iorg.index.values, diur_gheyl_iorg, label = 'Gheylla GOES-16 data')
+hours = np.linspace(00,23, 24)
+lt = [20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+fig, ax = plt.subplots(layout='constrained', figsize = (15,10))
+
+
+ax.plot(diur_hauk_iorg.hour.values, diur_hauk_iorg, label = 'Hauke CONT3XT data', color = 'red')
+ax.plot(diur_gheyl_iorg_280290.index.values, diur_gheyl_iorg_280290, label = 'Gheylla GOES-16 data mask 280-290K', color = 'black')
+ax.plot(diur_gheyl_iorg_277290.index.values, diur_gheyl_iorg_277290, label = 'Gheylla GOES-16 data mask 277-290K', color = 'black', linestyle = 'dashed')
+
+ax.grid()
+ax.set_xticks(hours)
+ax.set_xlabel('UTC Time [hr]')
+secax = ax.secondary_xaxis('top')
+secax.set_xticks(hours, lt)
+secax.set_xlabel('Local Time [hr]')
+
 plt.title('Diurnal cycle of Iorg')
-plt.xlabel('Time')
 plt.ylabel('Iorg [-]')
-plt.grid()
 plt.legend()
 
-plt.figure(figsize = (25,10))
-plt.plot(diur_hauk_iorg.hour.values, diur_hauk_num, label = 'Hauke CONT3XT data')
-plt.plot(diur_gheyl_iorg.index.values, diur_gheyl_num, label = 'Gheylla GOES-16 data')
+#%%
+hours = np.linspace(00,23, 24)
+lt = [20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+fig, ax = plt.subplots(layout='constrained', figsize = (25,10))
+
+
+ax.plot(diur_hauk_iorg.hour.values, diur_hauk_num, label = 'Hauke CONT3XT data', color = 'red')
+ax.plot(diur_gheyl_iorg_280290.index.values, diur_gheyl_num_280290, label = 'Gheylla GOES-16 data mask 280-290K', color = 'black')
+ax.plot(diur_gheyl_iorg_277290.index.values, diur_gheyl_num_277290, label = 'Gheylla GOES-16 data mask 277-290K', color = 'black', linestyle = 'dashed')
+
+ax.grid()
+ax.set_xticks(hours)
+ax.set_xlabel('UTC Time [hr]')
+secax = ax.secondary_xaxis('top')
+secax.set_xticks(hours, lt)
+secax.set_xlabel('Local Time [hr]')
+
 plt.title('Diurnal cycle of number of objects')
-plt.xlabel('Time')
 plt.ylabel('Number Objects [-]')
-plt.grid()
 plt.legend()
 
-plt.figure(figsize = (25,10))
-plt.plot(diur_hauk_iorg.hour.values, diur_hauk_cf, label = 'Hauke CONT3XT data')
-plt.plot(diur_gheyl_iorg.index.values, diur_gheyl_cf, label = 'Gheylla GOES-16 data')
+#%%
+hours = np.linspace(00,23, 24)
+lt = [20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+fig, ax = plt.subplots(layout='constrained', figsize = (15,7))
+
+ax.plot(diur_hauk_iorg.hour.values, diur_hauk_cf, label = 'Hauke CONT3XT data', color = 'red')
+ax.plot(diur_gheyl_iorg_280290.index.values, diur_gheyl_cf_280290, label = 'Gheylla GOES-16 data mask 280-290K', color = 'black')
+ax.plot(diur_gheyl_iorg_277290.index.values, diur_gheyl_cf_277290, label = 'Gheylla GOES-16 data mask 277-290K', color = 'black', linestyle = 'dashed')
+
+ax.grid()
+ax.set_xticks(hours)
+ax.set_xlabel('UTC Time [hr]')
+secax = ax.secondary_xaxis('top')
+secax.set_xticks(hours, lt)
+secax.set_xlabel('Local Time [hr]')
+
 plt.title('Diurnal cycle of cloud fraction')
-plt.xlabel('Time')
 plt.ylabel('CF [-]')
-plt.grid()
 plt.legend()
 
 
 
-# %%
-'''Compute the mean cluster size'''
-S_gheyl = metrics_gheyl['fractal_dimension'][1:8879] / metrics_gheyl['num_objects'][1:8879]
-
-plt.figure(figsize = (25,10))
-plt.plot(metrics_hauk.time.values, metrics_hauk.cluster_size_mean.values, label = 'Hauke CONT3XT data')
-plt.plot(metrics_gheyl.Time.values[1:8879], S_gheyl, label = 'Gheylla GOES-16 data')
-plt.title('Timeseries of mean cluster size')
-plt.xlabel('Time')
-plt.ylabel('Mean size[-]')
-plt.grid()
-plt.legend()
-
-plt.figure(figsize = (25,10))
-plt.plot(diur_hauk_iorg.hour.values, diur_hauk_size, label = 'Hauke CONT3XT data')
-plt.title('Diurnal cycle of mean size')
-plt.xlabel('Time')
-plt.ylabel('Cluster size [-]')
-plt.grid()
-plt.legend()
