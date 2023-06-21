87c6fb1d0b74d3f0f99e2a75b43753a004b02e6d
Gheylla <104219161+Gheylla@users.noreply.github.com>
2023-06-21 10:32:29 +0200

    Update metrics_HARMONIE_with_OBS.py

--- metrics_HARMONIE_with_OBS.py
+++ metrics_HARMONIE_with_OBS.py
@@ -84,14 +84,14 @@ for ii in range(len(keys)-1):
     diur_nohgtqs_noshal = harm_nohgtqs_noshal[keys[ii]].groupby(harm_nohgtqs_noshal.index.hour).mean()
     diur_nohgtqs_nouvmix = harm_nohgtqs_nouvmix[keys[ii]].groupby(harm_nohgtqs_nouvmix.index.hour).mean()
     
-    fig, ax = plt.subplots(layout='constrained', figsize = (15,10))
+    fig, ax = plt.subplots(layout='constrained', figsize = (15,5))
     
 
     ax.plot(diur_nohgtqs.index.values, diur_nohgtqs, color = 'red', label = 'HARMONIE noHGTQS' )
     ax.plot(diur_nohgtqs_noshal.index.values, diur_nohgtqs_noshal, color = 'blue', label = 'HARMONIE noHGTQS noSHAL' )
     ax.plot(diur_nohgtqs_nouvmix.index.values, diur_nohgtqs_nouvmix, color = 'green', label = 'HARMONIE noHGTQS noUVmix' )
-    ax.plot(diur_goes_277.index.values, diur_goes_277, color = 'black', label = 'Observations mask 277-290K')
-    ax.plot(diur_goes_280.index.values, diur_goes_280, color = 'black', linestyle = 'dashed', label = 'Observations mask 280-290K')
+    ax.plot(diur_goes_277.index.values, diur_goes_277, color = 'black', label = 'Observations GOES-16')
+    #ax.plot(diur_goes_280.index.values, diur_goes_280, color = 'black', linestyle = 'dashed', label = 'Observations mask 280-290K')
     
     ax.grid()
     ax.set_xticks(hours)
@@ -109,20 +109,98 @@ for ii in range(len(keys)-1):
 '''Make correlation plot of all metrics'''
 keys = harm_nohgtqs.keys()
 
-for jj in range(len(keys) - 1):
-    plt.figure()
-    plt.scatter(goes_metrics277[keys[jj]][1:8879], goes_metrics277[keys[jj + 1 ]][1:8879], s = 1, color = 'black')
-    plt.grid()
-    plt.xlabel(f'{keys[jj]}')
-    plt.ylabel(f'{keys[jj + 1]}')
-    plt.title('Correlation for GOES 16 metrics')
-
+# =============================================================================
+# for jj in range(len(keys) - 1):
+#     plt.figure()
+#     plt.scatter(goes_metrics277[keys[jj]][1:8879], goes_metrics277[keys[jj + 1 ]][1:8879], s = 1, color = 'black')
+#     plt.grid()
+#     plt.xlabel(f'{keys[jj]}')
+#     plt.ylabel(f'{keys[jj + 1]}')
+#     plt.title('Correlation for GOES 16 metrics')
+# 
+# 
+# plt.figure()
+# plt.scatter(goes_metrics277['mean_length_scale'][1:8879], goes_metrics277['open_sky'][1:8879], s = 1, color = 'black')
+# plt.grid()
+# plt.xlabel('mean length scale')
+# plt.ylabel('open sky')
+# plt.title('Correlation for GOES 16 metrics')
+# 
+# 
+# =============================================================================
+# =============================================================================
+# for jj in range(len(keys) - 1):
+#     plt.figure()
+#     plt.scatter(harm_nohgtqs[keys[jj]], harm_nohgtqs[keys[jj + 1 ]], s = 1, color = 'red')
+#     plt.grid()
+#     plt.xlabel(f'{keys[jj]}')
+#     plt.ylabel(f'{keys[jj + 1]}')
+#     plt.title('Correlation for GOES 16 metrics')
+# 
+# =============================================================================
 
 plt.figure()
-plt.scatter(goes_metrics277['mean_length_scale'][1:8879], goes_metrics277['open_sky'][1:8879], s = 1, color = 'black')
+plt.scatter(harm_nohgtqs['open_sky'], harm_nohgtqs['mean_length_scale'], s = 1, color = 'red')
 plt.grid()
-plt.xlabel('mean length scale')
-plt.ylabel('open sky')
-plt.title('Correlation for GOES 16 metrics')
+plt.xlabel('Iorg')
+plt.ylabel('Number of objects')
+plt.title('Correlation for HARMONIE noHGTQS metrics')
+plt.xlim([0,0.4])
 
-    
+plt.figure()
+plt.scatter(harm_nohgtqs_noshal['open_sky'], harm_nohgtqs_noshal['mean_length_scale'], s = 1, color = 'blue')
+plt.grid()
+plt.xlabel('Iorg')
+plt.ylabel('Number of objects')
+plt.title('Correlation for HARMONIE noHGTQS noSHAL metrics')
+#plt.ylim([0,1])
+#plt.xlim([0,0.1])
+
+plt.figure()
+plt.scatter(harm_nohgtqs_nouvmix['open_sky'], harm_nohgtqs_nouvmix['mean_length_scale'], s = 1, color = 'green')
+plt.grid()
+plt.xlabel('Iorg')
+plt.ylabel('Number of objects')
+plt.title('Correlation for HARMONIE noHGTQS noUVmix metrics')
+#plt.ylim([0,1])
+
+plt.figure()
+plt.scatter(goes_metrics277['open_sky'], goes_metrics277['mean_length_scale'], s = 1, color = 'black')
+plt.grid()
+plt.xlabel('Iorg')
+plt.ylabel('Number of objects')
+plt.title('Correlation for GOES-16 observation metrics')
+#plt.ylim([0,0.45])
+
+# %%
+keys = harm_nohgtqs.keys()
+hours = np.linspace(00,23, 24)
+lt = [20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+CLL_ceilo = [0.39941014, 0.34578938, 0.33543152, 0.34728219, 0.45542383, 0.37588653,  0.32376386, 0.30691307, 0.38209452, 0.39497858, 0.39855901, 0.36637728,  0.27261233, 0.33223915, 0.306564,   0.28822225, 0.2872085,  0.27971313,  0.32173261, 0.32721727, 0.32638889, 0.33046875, 0.31032944, 0.33919392]    
+diur_goes_277 = goes_metrics277['cloud_fraction'].groupby(goes_metrics277.index.hour).mean()
+
+diur_nohgtqs = harm_nohgtqs['cloud_fraction'].groupby(harm_nohgtqs.index.hour).mean()
+diur_nohgtqs_noshal = harm_nohgtqs_noshal['cloud_fraction'].groupby(harm_nohgtqs_noshal.index.hour).mean()
+diur_nohgtqs_nouvmix = harm_nohgtqs_nouvmix['cloud_fraction'].groupby(harm_nohgtqs_nouvmix.index.hour).mean()
+ 
+
+fig, ax = plt.subplots(layout='constrained', figsize = (15,5))
+
+ax.plot(diur_nohgtqs.index.values, diur_nohgtqs, color = 'red', label = 'HARMONIE noHGTQS' )
+ax.plot(diur_nohgtqs_noshal.index.values, diur_nohgtqs_noshal, color = 'blue', label = 'HARMONIE noHGTQS noSHAL' )
+ax.plot(diur_nohgtqs_nouvmix.index.values, diur_nohgtqs_nouvmix, color = 'green', label = 'HARMONIE noHGTQS noUVmix' )
+ax.plot(diur_goes_277.index.values, diur_goes_277, color = 'black', label = 'Observations GOES-16')
+ax.plot(diur_nohgtqs.index.values, CLL_ceilo, color = 'black', linestyle = 'dashed', label = 'Observations BCO ceilometer')
+
+
+ax.grid()
+ax.set_xticks(hours)
+ax.set_xlabel('UTC Time [hr]')
+secax = ax.secondary_xaxis('top')
+secax.set_xticks(hours, lt)
+secax.set_xlabel('Local Time [hr]')
+plt.ylabel('CF [-]')
+plt.title(f'Composite Diurnal cycle of cloud fraction')
+plt.legend()
+plt.show()
